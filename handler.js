d06f1f71a6a14dda6bc2880fea72f60a456c354c
amedinae <31489998+amedinae@users.noreply.github.com>
2023-10-03 09:56:06 +0200

    Add config and messages json files and schedule reminders

--- handler.js
+++ handler.js
@@ -1,18 +1,37 @@
-'use strict';
+"use strict";
+const axios = require("axios");
+const api = axios.create({ baseURL: "https://api.telegram.org" });
+const config = require("./config.json");
+const messages = require("./messages.json");
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const sendMessage = (message) => {
+    const token = config.token;
+    const chat_id = config.chat_id;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    const url = `/bot${token}/sendMessage`;
+    return api
+        .get(url, {
+            params: {
+                chat_id: chat_id,
+                text: message,
+            },
+        })
+        .then((_) => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: "Sent",
+                }),
+            };
+        });
 };
+
+module.exports.trainConfirmation = async (event) => {
+    return sendMessage(messages.trainConfirmation);
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.attendanceReminder = async (event) => {
+    return sendMessage(attendance_reminder);
+}
